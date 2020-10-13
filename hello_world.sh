44f610d549257a16d75545bcff54ba0669b3102d
Chad Jemmett <cjemmett@wehco.com>
2020-10-12 21:13:36 -0500

    Testing a script that checks for brew packages.

--- hello_world.sh
+++ hello_world.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "hello world!"
+# echo "hello world!"
+
+# this is a test to see if you can tell which packages brew has installed.
+
+if brew ls --versions pyenv > /dev/null; then
+    echo "The package is installed"
+else
+    echo "There is no pyenv"
+fi
